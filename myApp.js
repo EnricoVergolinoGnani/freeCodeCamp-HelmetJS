09730c127f8debe09f8c1cabac8895d09a9796dd
enricov <enricov@tecgraf.puc-rio.br>
2024-10-14 21:22:29 -0300

    Mitigating the Risk of Clickjacking with helmet.frameguard()

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ const helmet = require('helmet');
 // Using the helmet.hidePoweredBy() middleware to remove the X-Powered-By header.
 app.use(helmet.hidePoweredBy());
 
-
+// Using helmet.frameguard() to set the X-Frame-Options header as DENY
+app.use(helmet.frameguard({action: 'deny'}));
 
 
 
